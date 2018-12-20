517156cd6e65d93cd55be5de010a3e82ec3b9b60
Pedro Gomes Dias <pgdias020600@gmail.com>
2018-12-20 15:19:29 -0200

    Add files via upload

---
+++ Tarefa03.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Tarefa03) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+Exercício 33 -> problema do "ano 2000"
+R:O Bug do Milênio foi um acontecimento que ocorreu no final do século XX e designou uma preocupação mundial.
+O problema central do Bug do Milênio era o fato de que os sistemas antigos desenvolvidos no século XX guardavam e interpretavam as datas com 2 dígitos no ano.
+Isso ocorria pela necessidade de se economizar, uma vez que em 1965 um megabyte de espaço de memória magnética (suficiente para gravar um texto de 300 páginas) custava US$ 761.
+A partir daí, surgiu o medo de que após a virada do milênio, os sistemas reconhecessem o ano 2000 como 1900.
+Os Bancos teriam suas aplicações dando juros negativos, os investidores iriam ter enormes prejuízos, milhares de empresas iriam à falência, etc., significando uma crise maior ainda do que a de 1929.
+Mesmo que após o fato, tudo se normalizasse, o Bug do Milênio causaria uma enorme desordem no sistema econômico mundial.
+Assim, programadores aposentados foram chamados para estudar novamente os antigos sistemas. Além disso, ocorreu um grande esforço massivo em renovar os recursos de informática, gerando um enorme crescimento das empresas do ramo de informática.
+
+
+
+
+
+
